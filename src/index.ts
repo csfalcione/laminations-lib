557558e5e3549785461f3a5584b8b8d7c0cdb834
Caleb <caleb.falcione@gmail.com>
2019-08-10 15:29:41 -0700

    updates index with new exports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export {NaryFraction} from './nary'
 export {Chord} from './chord'
+export {Polygon} from './polygon'
+export {PullbackLamination, BranchRegion, makeRegion} from './pullback-lamination'
