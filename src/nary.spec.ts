bcd8fe6e1390349154edce09ab6b5810372a8e8c
Caleb <caleb.falcione@gmail.com>
2019-10-18 22:43:28 -0500

    improves nary::reduceCircularSequence()

--- src/nary.spec.ts
+++ src/nary.spec.ts
@@ -1,200 +1,201 @@
-import {NaryFraction, findCircularRepeatingSuffix, reduceCircularSequence} from './nary'
+import { NaryFraction, findCircularRepeatingSuffix, reduceCircularSequence } from './nary'
 
 
 describe('NaryFraction', () => {
-    const binary = NaryFraction.factory(2)
-    const ternary = NaryFraction.factory(3)
-    const quaternary = NaryFraction.factory(4)
-    const decimal = NaryFraction.factory(10)
-    const dozenal = NaryFraction.factory(12)
+  const binary = NaryFraction.factory(2)
+  const ternary = NaryFraction.factory(3)
+  const quaternary = NaryFraction.factory(4)
+  const decimal = NaryFraction.factory(10)
+  const dozenal = NaryFraction.factory(12)
 
-    it('support string interpolation', () => {
-      const num = NaryFraction.parse(3, '_102')
-      expect(`${num}`).toBe('_102')
-    })
+  it('support string interpolation', () => {
+    const num = NaryFraction.parse(3, '_102')
+    expect(`${num}`).toBe('_102')
+  })
 
-    it('supports nontrivial equals', () => {
+  it('supports nontrivial equals', () => {
 
-        const a1 = ternary([], [1, 0, 2])
-        const a2 = ternary([1], [0, 2, 1])
-        const a3 = ternary([1, 0], [2, 1, 0])
-        const a4 = ternary([1, 0, 2], [1, 0, 2])
-        const a5 = ternary([1, 0, 2, 1], [0, 2, 1])
-        const b  = ternary([2], [1])
+    const a1 = ternary([], [1, 0, 2])
+    const a2 = ternary([1], [0, 2, 1])
+    const a3 = ternary([1, 0], [2, 1, 0])
+    const a4 = ternary([1, 0, 2], [1, 0, 2])
+    const a5 = ternary([1, 0, 2, 1], [0, 2, 1])
+    const b = ternary([2], [1])
 
-        const c1 = ternary([1], [])
-        const c2 = decimal([], [3])
+    const c1 = ternary([1], [])
+    const c2 = decimal([], [3])
 
-        const d1 = ternary([], [0])
-        const d2 = binary([0, 0], [])
-        const d3 = decimal([], [9])
-        const d4 = binary([], [1])
+    const d1 = ternary([], [0])
+    const d2 = binary([0, 0], [])
+    const d3 = decimal([], [9])
+    const d4 = binary([], [1])
 
-        const e1 = decimal([4], [])
-        const e2 = decimal([3], [9])
-        const e3 = decimal([3, 9], [9])
+    const e1 = decimal([4], [])
+    const e2 = decimal([3], [9])
+    const e3 = decimal([3, 9], [9])
 
-        const f1 = ternary([1, 0, 0], [])
-        const f2 = ternary([1], [])
-        const f3 = ternary([1, 0, 0], [0, 0])
+    const f1 = ternary([1, 0, 0], [])
+    const f2 = ternary([1], [])
+    const f3 = ternary([1, 0, 0], [0, 0])
 
-        const g1 = ternary([2], [])
-        const g2 = binary([], [1, 0])
+    const g1 = ternary([2], [])
+    const g2 = binary([], [1, 0])
 
-        expect(a1.equals(a2)).toBe(true)
-        expect(a1.equals(a3)).toBe(true)
-        expect(a1.equals(a4)).toBe(true)
-        expect(a1.equals(a5)).toBe(true)
+    expect(a1.equals(a2)).toBe(true)
+    expect(a1.equals(a3)).toBe(true)
+    expect(a1.equals(a4)).toBe(true)
+    expect(a1.equals(a5)).toBe(true)
 
-        expect(a1.equals(b)).toBe(false)
+    expect(a1.equals(b)).toBe(false)
 
-        expect(c1.equals(c2)).toBe(true)
+    expect(c1.equals(c2)).toBe(true)
 
-        expect(d1.equals(d2)).toBe(true)
-        expect(d1.equals(d3)).toBe(true)
-        expect(d1.equals(d4)).toBe(true)
-        expect(d2.equals(d4)).toBe(true)
+    expect(d1.equals(d2)).toBe(true)
+    expect(d1.equals(d3)).toBe(true)
+    expect(d1.equals(d4)).toBe(true)
+    expect(d2.equals(d4)).toBe(true)
 
-        expect(e1.equals(e2)).toBe(true)
-        expect(e1.equals(e3)).toBe(true)
+    expect(e1.equals(e2)).toBe(true)
+    expect(e1.equals(e3)).toBe(true)
 
-        expect(f1.equals(f2)).toBe(true)
-        expect(f1.equals(f3)).toBe(true)
+    expect(f1.equals(f2)).toBe(true)
+    expect(f1.equals(f3)).toBe(true)
 
-        expect(g1.equals(g2)).toBe(true)
+    expect(g1.equals(g2)).toBe(true)
 
-    })
+  })
 
-    it('supports less-than', () => {
-        const a = ternary([1, 0, 2], [])
-        const b = ternary([], [1, 0, 2])
+  it('supports less-than', () => {
+    const a = ternary([1, 0, 2], [])
+    const b = ternary([], [1, 0, 2])
 
-        expect(a.lessThan(b)).toBe(true)
-        expect(b.lessThan(a)).toBe(false)
+    expect(a.lessThan(b)).toBe(true)
+    expect(b.lessThan(a)).toBe(false)
 
-        expect(a.lessThan(a)).toBe(false)
-        expect(b.lessThan(b)).toBe(false)
-    })
+    expect(a.lessThan(a)).toBe(false)
+    expect(b.lessThan(b)).toBe(false)
+  })
 
-    it('supports indexing', () => {
-        const a = ternary([1, 0, 2], [])
-        const b = ternary([1, 0], [1, 0, 2])
+  it('supports indexing', () => {
+    const a = ternary([1, 0, 2], [])
+    const b = ternary([1, 0], [1, 0, 2])
 
-        expect(a.digitAt(0)).toBe(1)
-        expect(a.digitAt(2)).toBe(2)
-        expect(a.digitAt(3)).toBe(0)
+    expect(a.digitAt(0)).toBe(1)
+    expect(a.digitAt(2)).toBe(2)
+    expect(a.digitAt(3)).toBe(0)
 
-        expect(b.digitAt(1)).toBe(0)
-        expect(b.digitAt(2)).toBe(1)
-        expect(b.digitAt(19)).toBe(2)
-    })
+    expect(b.digitAt(1)).toBe(0)
+    expect(b.digitAt(2)).toBe(1)
+    expect(b.digitAt(19)).toBe(2)
+  })
 
-    it('simplifies fractions', () => {
-        const f = NaryFraction.simplify
+  it('simplifies fractions', () => {
+    const f = NaryFraction.simplify
 
-        expect( f([1], [0, 2, 1]) ).toEqual([ [], [1, 0, 2] ])
-        expect( f([], [3, 3, 3]) ).toEqual([ [], [3] ])
+    expect(f(3, [1], [0, 2, 1])).toEqual([[], [1, 0, 2]])
+    expect(f(4, [], [3, 3, 3])).toEqual([[], [3]])
 
-        expect( f(   [3, 1, 1, 0, 2, 1, 0, 2, 1], [0, 2, 1]) )
-          .toEqual([ [3, 1],                      [1, 0, 2]] )
-        expect( f(   [3, 1, 1, 0, 2, 1, 0, 2, 1], [0, 2, 1, 0, 2, 1]) )
-          .toEqual([ [3, 1],                      [1, 0, 2]] )
-    })
+    expect(f(4, [3, 1, 1, 0, 2, 1, 0, 2, 1], [0, 2, 1]))
+      .toEqual([[3, 1], [1, 0, 2]])
+    expect(f(4, [3, 1, 1, 0, 2, 1, 0, 2, 1], [0, 2, 1, 0, 2, 1]))
+      .toEqual([[3, 1], [1, 0, 2]])
+  })
 
-    it('converts to rational numbers', () => {
-        expect( ternary([1], []).toRational() )
-          .toEqual([1, 3])
+  it('converts to rational numbers', () => {
+    expect(ternary([1], []).toRational())
+      .toEqual([1, 3])
 
-        expect( decimal([], [3]).toRational() )
-          .toEqual([1, 3])
+    expect(decimal([], [3]).toRational())
+      .toEqual([1, 3])
 
-        expect( binary([1], [1, 0, 1]).toRational())
-          .toEqual([6, 7])
+    expect(binary([1], [1, 0, 1]).toRational())
+      .toEqual([6, 7])
 
-        expect( quaternary([3, 1], [1, 0, 2]).toRational() )
-          .toEqual([93, 112])
-        
-        expect( ternary([], []).toRational() )
-          .toEqual([0, 1])
+    expect(quaternary([3, 1], [1, 0, 2]).toRational())
+      .toEqual([93, 112])
 
-        expect( ternary([2], []).toRational() )
-          .toEqual([2, 3])
+    expect(ternary([], []).toRational())
+      .toEqual([0, 1])
 
-        expect( binary([], [1, 0]).toRational() )
-          .toEqual([2, 3])
-    })
+    expect(ternary([2], []).toRational())
+      .toEqual([2, 3])
 
-    it('supports a forward map', () => {
-      expect(
-        ternary([], [])
+    expect(binary([], [1, 0]).toRational())
+      .toEqual([2, 3])
+  })
+
+  it('supports a forward map', () => {
+    expect(
+      ternary([], [])
         .mapForward()
-      ).toEqual(ternary([], []));
+    ).toEqual(ternary([], []));
 
-      expect(
-        ternary([], [0, 1, 2])
+    expect(
+      ternary([], [0, 1, 2])
         .mapForward()
-      ).toEqual(ternary([], [1, 2, 0]));
+    ).toEqual(ternary([], [1, 2, 0]));
 
-      expect(
-        ternary([1], [1, 2])
+    expect(
+      ternary([1], [1, 2])
         .mapForward()
-      ).toEqual(ternary([], [1, 2]));
+    ).toEqual(ternary([], [1, 2]));
 
-      expect(
-        ternary([1, 2], []).mapForward()
+    expect(
+      ternary([1, 2], []).mapForward()
         .equals(binary([], [0, 1]).mapForward())
-      ).toBe(true)
-    })
+    ).toBe(true)
+  })
 
-    it('supports a backwards map', () => {
-      expect(ternary([], [0, 1]).mapBackward())
+  it('supports a backwards map', () => {
+    expect(ternary([], [0, 1]).mapBackward())
       .toEqual([
         ternary([0], [0, 1]),
         ternary([1], [0, 1]),
         ternary([2], [0, 1])
       ])
-    })
+  })
 
-    it('supports parsing string input', () => {
-      const parseTernary = NaryFraction.parseFactory(3)
-      const parseDozenal = NaryFraction.parseFactory(12)
+  it('supports parsing string input', () => {
+    const parseTernary = NaryFraction.parseFactory(3)
+    const parseDozenal = NaryFraction.parseFactory(12)
 
-      expect(parseTernary('100').equals(ternary([1, 0, 0], [])))
-      expect(parseTernary('100_').equals(ternary([1, 0, 0], [])))
-      expect(parseTernary('_100').equals(ternary([], [1, 0, 0])))
-      expect(parseTernary('1_100').equals(ternary([1], [1, 0, 0])))
-      
-      expect(parseDozenal('11,9,2').equals(dozenal([12, 9, 2], [])))
-      expect(parseDozenal('11,9,2_').equals(dozenal([12, 9, 2], [])))
-      expect(parseDozenal('_11,9,2').equals(dozenal([], [11, 9, 2])))
-      expect(parseDozenal('11_11,9,2').equals(dozenal([11], [11, 9, 2])))
-    })
+    expect(parseTernary('100').equals(ternary([1, 0, 0], [])))
+    expect(parseTernary('100_').equals(ternary([1, 0, 0], [])))
+    expect(parseTernary('_100').equals(ternary([], [1, 0, 0])))
+    expect(parseTernary('1_100').equals(ternary([1], [1, 0, 0])))
+
+    expect(parseDozenal('11,9,2').equals(dozenal([11, 9, 2], [])))
+    expect(parseDozenal('11,9,2_').equals(dozenal([11, 9, 2], [])))
+    expect(parseDozenal('_11,9,2').equals(dozenal([], [11, 9, 2])))
+    expect(parseDozenal('11_11,9,2').equals(dozenal([11], [11, 9, 2])))
+  })
 
 })
 
 describe('findCircularRepeatingSuffix()', () => {
-    const f = findCircularRepeatingSuffix
+  const f = findCircularRepeatingSuffix
 
-    it('handles edge cases', () => {
-        expect(f([3], [0, 2, 1])).toEqual(1)
-        expect(f([3], [])).toEqual(1)
-        expect(f([], [1, 2])).toEqual(0)
-        expect(f([], [])).toEqual(0)
-    })
+  it('handles edge cases', () => {
+    expect(f([3], [0, 2, 1])).toEqual(1)
+    expect(f([3], [])).toEqual(1)
+    expect(f([], [1, 2])).toEqual(0)
+    expect(f([], [])).toEqual(0)
+  })
 
-    it('handles normal cases', () => {
-        expect(f([1], [0, 2, 1])).toEqual(0)
-        expect(f([3, 1, 1], [0, 2, 1])).toEqual(2)
-        expect(f([3, 1, 1, 0, 2, 1], [0, 2, 1])).toEqual(2)
-        expect(f([3, 1, 1, 0, 2, 1, 0, 2, 1], [0, 2, 1])).toEqual(2)
-    })
+  it('handles normal cases', () => {
+    expect(f([1], [0, 2, 1])).toEqual(0)
+    expect(f([3, 1, 1], [0, 2, 1])).toEqual(2)
+    expect(f([3, 1, 1, 0, 2, 1], [0, 2, 1])).toEqual(2)
+    expect(f([3, 1, 1, 0, 2, 1, 0, 2, 1], [0, 2, 1])).toEqual(2)
+  })
 })
 
 describe('reduceCircularSequence()', () => {
-    const f = reduceCircularSequence
-    it('works', () => {
-        expect(f([3, 3, 3])).toEqual([3])
-        expect(f([1, 2, 3])).toEqual([1, 2, 3])
-        expect(f([1, 2, 3, 1, 2, 3])).toEqual([1, 2, 3])
-    })
+  const f = reduceCircularSequence
+  it('works', () => {
+    expect(f(4, [3, 3, 3])).toEqual([3])
+    expect(f(4, [1, 2, 3])).toEqual([1, 2, 3])
+    expect(f(4, [1, 2, 3, 1, 2, 3])).toEqual([1, 2, 3])
+    expect(f(2, [1, 0, 1])).toEqual([1, 0, 1])
+  })
 })
--- src/nary.ts
+++ src/nary.ts
@@ -13,10 +13,10 @@ export class NaryFraction {
 
     constructor(
         base: number,
-        exactPart: Array<number>,
-        repeatingPart: Array<number>
+        exactPart: number[],
+        repeatingPart: number[]
     ) {
-        let [newExact, newRepeating] = NaryFraction.simplify(exactPart, repeatingPart)
+        let [newExact, newRepeating] = NaryFraction.simplify(base, exactPart, repeatingPart)
 
         if (arraysEqual(newRepeating, [base - 1])) {
             newRepeating = []
@@ -37,18 +37,18 @@ export class NaryFraction {
     }
 
     public base: number
-    public exactPart: Array<number>
-    public repeatingPart: Array<number>
+    public exactPart: number[]
+    public repeatingPart: number[]
 
     public static new(
         base: number,
-        exactPart: Array<number>,
-        repeatingPart: Array<number>): NaryFraction {
+        exactPart: number[],
+        repeatingPart: number[]): NaryFraction {
         return new NaryFraction(base, exactPart, repeatingPart)
     }
 
     public static factory(base: number) {
-        return (exactPart: Array<number>, repeatingPart: Array<number>): NaryFraction => {
+        return (exactPart: number[], repeatingPart: number[]): NaryFraction => {
             return new NaryFraction(base, exactPart, repeatingPart)
         }
     }
@@ -62,7 +62,7 @@ export class NaryFraction {
         const digitSplitter = base < 10 ? '' : ','
 
         const exactPart = exactText.split(digitSplitter).map(x => parseInt(x))
-        const repeatingPart = repeatingText.split(digitSplitter).map(x => parseInt(x))
+        const repeatingPart = repeatingText.split(digitSplitter).filter(x => x.length > 0).map(x => parseInt(x))
 
         return new NaryFraction(base, exactPart, repeatingPart)
     }
@@ -71,8 +71,8 @@ export class NaryFraction {
         return (text: string): NaryFraction => NaryFraction.parse(base, text)
     }
 
-    public static simplify(exactPart: Array<number>, repeatingPart: Array<number>): [Array<number>, Array<number>] {
-        repeatingPart = reduceCircularSequence(repeatingPart)
+    public static simplify(base: number, exactPart: number[], repeatingPart: number[]): [number[], number[]] {
+        repeatingPart = reduceCircularSequence(base, repeatingPart)
         const repeatingSuffixStart = findCircularRepeatingSuffix(exactPart, repeatingPart)
         const newExactPart = exactPart.slice(0, repeatingSuffixStart)
 
@@ -110,7 +110,7 @@ export class NaryFraction {
         return fraction.mapForward();
     }
     
-    public static mapBackward(fraction: NaryFraction): Array<NaryFraction> {
+    public static mapBackward(fraction: NaryFraction): NaryFraction[] {
         return fraction.mapBackward();
     }
 
@@ -180,7 +180,7 @@ export class NaryFraction {
         )
     }
 
-    public mapBackward(): Array<NaryFraction> {
+    public mapBackward(): NaryFraction[] {
         const base = this.base
         const exact = this.exactPart
         let result = new Array(base)
@@ -209,25 +209,18 @@ export class NaryFraction {
 
     public numerator(): number {
         const d = this.base
-        return this.repeatingDenominator() * valueFromDigits(d, this.exactPart)
+        return repeatingDenominator(d, this.exactPart) * valueFromDigits(d, this.exactPart)
             + valueFromDigits(d, this.repeatingPart)
     }
 
     public denominator(): number {
-        return this.repeatingDenominator() * Math.pow(this.base, this.exactPart.length)
+        const unshifted = repeatingDenominator(this.base, this.repeatingPart)
+        return unshifted * Math.pow(this.base, this.exactPart.length)
     }
 
     public toString() {
         const joiner = this.base < 10 ? '' : ','
         return `${this.exactPart.join(joiner)}_${this.repeatingPart.join(joiner)}`
-    }
-
-    private repeatingDenominator(): number {
-        const result = Math.pow(this.base, this.repeatingPart.length) - 1
-        if (result === 0) {
-            return 1
-        }
-        return result
     }
 
 }
@@ -236,32 +229,39 @@ export class NaryFraction {
 const mod = (a: number, b: number): number => ((a % b) + b) % b
 
 /**
- * Finds the shortest-length contiguous subsequence w of 'sequence' such that
+ * Finds the shortest-length prefix w of 'sequence' such that
  * 'sequence' is some number of concatenations of w.
  * Ex:
  * [1, 1, 1] -> [1]
  * [1, 2, 3, 1, 2, 3] -> [1, 2, 3]
  * [1, 2, 3, 1, 2] -> [1, 2, 3, 1, 2]
  */
-export const reduceCircularSequence = <T>(sequence: Array<T>): Array<T> => {
+export const reduceCircularSequence = (base: number, sequence: number[]): number[] => {
+    const [seqNumerator, seqDenominator] = rationalFromDigits(base, sequence)
     for (let width = 1; width <= sequence.length / 2; width++) {
         if (sequence.length % width !== 0) {
             continue
         }
-        if (isCircularOfWidth(sequence, width)) {
-            return sequence.slice(0, width)
+        const prefix = sequence.slice(0, width)
+        const [prefixNumerator, prefixDenominator] = rationalFromDigits(base, prefix)
+        if (seqNumerator === prefixNumerator && seqDenominator == prefixDenominator) {
+            return prefix
         }
     }
 
     return sequence
 }
 
-const isCircularOfWidth = <T>(sequence: Array<T>, width: number): boolean => {
-    return range(0, width)
-        .every(startIdx => range(startIdx, sequence.length, width)
-            .map(idx => sequence[idx])
-            .every(val => val === sequence[startIdx])
-        )
+const rationalFromDigits = (base: number, digits: number[]): [number, number] => {
+    const numerator = valueFromDigits(base, digits)
+    const denominator = repeatingDenominator(base, digits)
+    const gcd = greatestCommonDivisor(numerator, denominator)
+    return [Math.round(numerator / gcd), Math.round(denominator / gcd)]
+
+}
+
+const repeatingDenominator = (base: number, digits: number[]): number => {
+    return digits.length > 0 ? Math.pow(base, digits.length) - 1 : 1
 }
 
 /**
@@ -276,7 +276,7 @@ const isCircularOfWidth = <T>(sequence: Array<T>, width: number): boolean => {
  * ([3], []) -> 1
  * ([], []) -> 0
  */
-export const findCircularRepeatingSuffix = <T>(sequence: Array<T>, repeating: Array<T>): number => {
+export const findCircularRepeatingSuffix = <T>(sequence: T[], repeating: T[]): number => {
     const indexSequenceFromRight = idx => sequence.length - idx - 1
     const indexRepeatingFromRight = idx => repeating.length - (idx % repeating.length) - 1
 
@@ -292,7 +292,7 @@ export const findCircularRepeatingSuffix = <T>(sequence: Array<T>, repeating: Ar
 }
 
 // ex: ([1,2,3], 1) -> [3,1,2]
-const rotateRight = <T>(array: Array<T>, offset: number): Array<T> => {
+const rotateRight = <T>(array: T[], offset: number): T[] => {
     const len = array.length
     const result = new Array<T>(len)
 
@@ -303,11 +303,11 @@ const rotateRight = <T>(array: Array<T>, offset: number): Array<T> => {
     return result
 }
 
-const rotateLeft = <T>(array: Array<T>, offset: number): Array<T> => {
+const rotateLeft = <T>(array: T[], offset: number): T[] => {
     return rotateRight(array, -1 * offset)
 }
 
-const arraysEqual = <T>(a: Array<T>, b: Array<T>): boolean => {
+const arraysEqual = <T>(a: T[], b: T[]): boolean => {
     if (a.length !== b.length) {
         return false
     }
@@ -321,18 +321,8 @@ const arraysEqual = <T>(a: Array<T>, b: Array<T>): boolean => {
     return true
 }
 
-const range = (start: number, end: number, step: number = 1): Array<number> => {
-    const result = []
 
-    for (let num = start; num < end; num += step) {
-        result.push(num)
-    }
-
-    return result
-}
-
-
-const valueFromDigits = (base: number, digits: Array<number>): number => {
+const valueFromDigits = (base: number, digits: number[]): number => {
     let sum = 0
     let place = 1
     for (let idx = digits.length - 1; idx >= 0; idx--) {
@@ -343,7 +333,7 @@ const valueFromDigits = (base: number, digits: Array<number>): number => {
     return sum
 }
 
-const incrementDigitSequence = (base: number, digits: Array<number>): Array<number> => {
+const incrementDigitSequence = (base: number, digits: number[]): number[] => {
     const copy = [...digits]
     let carry = 1
     let idx = digits.length - 1
@@ -361,7 +351,7 @@ const incrementDigitSequence = (base: number, digits: Array<number>): Array<numb
     return copy
 }
 
-const removeTrailingZeroes = (array: Array<number>): Array<number> => {
+const removeTrailingZeroes = (array: number[]): number[] => {
     let idx = array.length - 1
 
     while (idx >= 0 && array[idx] === 0) {
